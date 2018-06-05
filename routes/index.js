39fc32f45db11b7bcd87be9f1da86ebb3cbd4ae9
WictorBertulino <wictorbertulino@gmail.com>
2018-06-05 19:48:50 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Olá Bem vindo ao sistema', Helow:"Cadastre o ip da sua conexão" });
 });
 router.post('/inserir', function(req, res, next) {
- 
+ inserir();
 });
+
+
+function inserir(){
+  var mysql = require('mysql');
+  
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO table (id  ,ipConexao) VALUES ( '498751598498165')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+}
 module.exports = router;
